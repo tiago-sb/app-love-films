4a0ac4d71a59486fbc0b6a41e695ff14c9586462
tiago-sb <belatiago72@gmail.com>
2024-02-15 18:33:46 -0300

    adiciona botão que leva o usuário para a parte superior do site

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,13 @@ hamburguer.addEventListener("click", () => {
     if(clicado) { botao.classList.remove('navegacao-escolhida') }
     else { botao.classList.add('navegacao-escolhida') }
 })
+
+document.querySelector(".paraCima").addEventListener("click", () => {
+    window.location.href = '#home'
+})
+
+document.addEventListener("scroll", () => {
+    const botao = document.querySelector(".paraCima")
+    if(window.scrollY > 600) { botao.style.display = 'flex'; }
+    else { botao.style.display = "none";  }
+})
