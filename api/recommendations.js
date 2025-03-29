a54bc4d55d53dfdffa70b5e35160c4f9e555224d
tiago-sb <belatiago72@gmail.com>
2025-03-29 20:24:06 -0300

    .

--- api/recommendations.js
+++ api/recommendations.js
@@ -6,17 +6,16 @@ const tmdbUrl = 'https://api.themoviedb.org/3/movie'
 module.exports = async (req, res) => {
   const { id } = req.query
 
+  const response = await axios.get(`${tmdbUrl}/${id}/recommendations`, {
+    params: { language: 'pt-BR', page: 1 },
+    headers: { Authorization: `Bearer ${apiKey}` }
+  })
   try {
-    const response = await axios.get(`${tmdbUrl}/${id}/recommendations`, {
-      params: { language: 'pt-BR', page: 1 },
-      headers: { Authorization: `Bearer ${apiKey}` }
-    })
-
     res.json(response.data)
   } catch (error) {
     console.error(error)
     res.status(500).json({ 
-      error: `Erro ao buscar recomendações do filme com id ${id} - ${tmdbUrl}/${id}/recommendations`
+      error: `Erro ao buscar recomendações do filme com id ${id} - ${response}`
     })
   }
 } 
